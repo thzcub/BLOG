4920e78955ed5a7dd4515ae35bc805b99768d2a3
thales <thalesa202@gmail.com>
2023-12-14 19:09:27 -0300

    end upgrade

--- djangoapp/project/wsgi.py
+++ djangoapp/project/wsgi.py
@@ -7,9 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
+
 import os
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# DOTENV
+load_dotenv(BASE_DIR.parent / 'dotenv_files' / '.env', override=True)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
